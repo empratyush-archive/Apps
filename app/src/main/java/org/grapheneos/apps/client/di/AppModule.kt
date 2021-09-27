b344323762d0427e1a2ecce9228c3282f391469e
Pratyush <codelab@pratyush.dev>
2021-09-27 06:59:43 +0530

    added ApkDownloadHelper and MetaDataHelper as Singleton Injectable

---
+++ app/src/main/java/org/grapheneos/apps/client/di/AppModule.kt
@@ -0,0 +1,41 @@
+package org.grapheneos.apps.client.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import org.grapheneos.apps.client.utils.network.ApkDownloadHelper
+import org.grapheneos.apps.client.utils.network.MetaDataHelper
+import java.time.Duration
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providerOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .callTimeout(Duration.ofMinutes(1))
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideMetaDataHelper(
+        okHttpClient: OkHttpClient,
+        @ApplicationContext context: Context
+    ): MetaDataHelper = MetaDataHelper(okHttpClient, context)
+
+    @Singleton
+    @Provides
+    fun provideApkDownloadHelper(
+        okHttpClient: OkHttpClient,
+        @ApplicationContext context: Context
+    ): ApkDownloadHelper = ApkDownloadHelper(okHttpClient, context)
+
+}
