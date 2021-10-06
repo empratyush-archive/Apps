cd14a7a914be1156b95945a3819092e04fdc7822
Pratyush <codelab@pratyush.dev>
2021-10-06 06:12:22 +0530

    removed okHttp from ApkDownloadHelper.kt

--- app/src/main/java/org/grapheneos/apps/client/utils/network/ApkDownloadHelper.kt
+++ app/src/main/java/org/grapheneos/apps/client/utils/network/ApkDownloadHelper.kt
@@ -5,20 +5,9 @@ import android.content.Context
 import androidx.annotation.RequiresPermission
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.ResponseBody
-import okio.Source
-import okio.Buffer
-import okio.ForwardingSource
-import okio.BufferedSink
-import okio.BufferedSource
-import okio.buffer
-import okio.sink
+import org.grapheneos.apps.client.di.DaggerHttpHelperComponent
 import org.grapheneos.apps.client.item.PackageVariant
 import java.io.File
-import java.io.FileOutputStream
 import java.io.IOException
 import java.net.UnknownHostException
 import java.security.GeneralSecurityException
@@ -26,12 +15,10 @@ import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import javax.net.ssl.SSLHandshakeException
 
-class ApkDownloadHelper constructor(
-    private val okHttpClient: OkHttpClient,
-    private val context: Context
-) {
+class ApkDownloadHelper constructor(private val context: Context) {
 
-    @Throws(UnknownHostException::class,
+    @Throws(
+        UnknownHostException::class,
         GeneralSecurityException::class,
         IOException::class,
         SSLHandshakeException::class
@@ -52,47 +39,26 @@ class ApkDownloadHelper constructor(
         variant.packagesInfo.forEach { (fileName, sha256Hash) ->
             withContext(Dispatchers.IO) {
                 val downloadedFile = File(downloadDir.absolutePath, fileName)
+                val uri =
+                    "https://apps.grapheneos.org/packages/${variant.pkgName}/${variant.versionCode}/${fileName}"
 
-                val shouldResume = downloadedFile.exists()
-                val downloadedSize = if(shouldResume) downloadedFile.readBytes().size else 0
-
-                val request = Request.Builder()
-                    .url("https://apps.grapheneos.org/packages/${variant.pkgName}/${variant.versionCode}/${fileName}")
-                    .build()
-
-                //if file already exist and it match given hash instead of downloading it again just return it as it is
-                // but only if it match with given hash otherwise remove it and download it fresh
                 if (downloadedFile.exists() && verifyHash(downloadedFile, sha256Hash)) {
                     result.add(downloadedFile)
                 } else {
-                    val response = okHttpClient.newBuilder()
-                        .addInterceptor { chain ->
-                            val originalResponse = chain.proceed(chain.request())
-                            try {
-                                originalResponse.body?.let { body ->
-                                    return@addInterceptor originalResponse.newBuilder()
-                                        .body(ProgressResponseBody(body, progressListener))
-                                        .build()
-                                }
-                            } catch (ignored: IOException) {
-                                println(ignored.localizedMessage)
-                            }
-                            originalResponse
-                        }.build().newCall(request).execute()
-                    response.body?.let { body ->
-                        if(shouldResume){
-                            body.source().skip(downloadedSize.toLong())
-                        }
-                        val sink: BufferedSink = FileOutputStream(downloadedFile, shouldResume).sink().buffer()
-                        sink.writeAll(body.source())
-                        sink.close()
 
-                        if (!verifyHash(downloadedFile, sha256Hash)) {
-                            downloadedFile.delete()
-                            throw GeneralSecurityException("Hash didn't matched")
-                        }
-                        result.add(downloadedFile)
+                    DaggerHttpHelperComponent.builder()
+                        .file(downloadedFile)
+                        .uri(uri)
+                        .build()
+                        .downloader()
+                        .saveToFile()
+
+                    if (!verifyHash(downloadedFile, sha256Hash)) {
+                        downloadedFile.delete()
+                        throw GeneralSecurityException("Hash didn't matched")
                     }
+                    result.add(downloadedFile)
+
                 }
 
             }
@@ -121,40 +87,5 @@ class ApkDownloadHelper constructor(
         return sb.toString()
     }
 
-    inner class ProgressResponseBody(
-        private val responseBody: ResponseBody,
-        private var listener: (read: Long, total: Long, doneInPercent: Long, taskCompleted: Boolean) -> Unit
-    ) : ResponseBody() {
-        private val bufferedSource: BufferedSource = source(responseBody.source()).buffer()
-        override fun contentType(): MediaType? {
-            return responseBody.contentType()
-        }
-
-        override fun contentLength(): Long {
-            return responseBody.contentLength()
-        }
-
-        override fun source(): BufferedSource = bufferedSource
-
-        private fun source(source: Source): Source {
-            return object : ForwardingSource(source) {
-                var totalBytesRead = 0L
-
-                @Throws(IOException::class)
-                override fun read(sink: Buffer, byteCount: Long): Long {
-                    val bytesRead = super.read(sink, byteCount)
-                    // read() returns the number of bytes read, or -1 if this source is exhausted.
-                    totalBytesRead += if (bytesRead != -1L) bytesRead else 0
-                    listener.invoke(
-                        totalBytesRead,
-                        responseBody.contentLength(),
-                        (totalBytesRead * 100) / responseBody.contentLength(),
-                        bytesRead == -1L
-                    )
-                    return bytesRead
-                }
-            }
-        }
-    }
 }
 
