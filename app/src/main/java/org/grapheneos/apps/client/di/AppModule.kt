6b12e3a4e8bb369b50a20e89e27de89bd738211b
Pratyush <codelab@pratyush.dev>
2021-10-06 06:12:03 +0530

    removed okHttp from AppModule.kt

--- app/src/main/java/org/grapheneos/apps/client/di/AppModule.kt
+++ app/src/main/java/org/grapheneos/apps/client/di/AppModule.kt
@@ -6,10 +6,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
 import org.grapheneos.apps.client.utils.network.ApkDownloadHelper
 import org.grapheneos.apps.client.utils.network.MetaDataHelper
-import java.time.Duration
 import javax.inject.Singleton
 
 @Module
@@ -18,28 +16,13 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun providerOkHttpClient(): OkHttpClient =
-        OkHttpClient.Builder()
-            .retryOnConnectionFailure(true)
-            .readTimeout(Duration.ofSeconds(30))
-            .writeTimeout(Duration.ofSeconds(30))
-            .callTimeout(Duration.ofSeconds(30))
-            .connectTimeout(Duration.ofSeconds(30))
-            .pingInterval(Duration.ofSeconds(10))
-            .build()
-
-    @Singleton
-    @Provides
-    fun provideMetaDataHelper(
-        okHttpClient: OkHttpClient,
-        @ApplicationContext context: Context
-    ): MetaDataHelper = MetaDataHelper(okHttpClient, context)
+    fun provideMetaDataHelper(@ApplicationContext context: Context
+    ): MetaDataHelper = MetaDataHelper(context)
 
     @Singleton
     @Provides
     fun provideApkDownloadHelper(
-        okHttpClient: OkHttpClient,
         @ApplicationContext context: Context
-    ): ApkDownloadHelper = ApkDownloadHelper(okHttpClient, context)
+    ): ApkDownloadHelper = ApkDownloadHelper(context)
 
 }
