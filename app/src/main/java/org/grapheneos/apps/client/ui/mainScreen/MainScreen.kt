b77ba9597f8962ecc2ab0cd730cd71b565872354
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2021-12-23 15:06:48 +0000

    remove wildcard import

--- app/src/main/java/org/grapheneos/apps/client/ui/mainScreen/MainScreen.kt
+++ app/src/main/java/org/grapheneos/apps/client/ui/mainScreen/MainScreen.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.*
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DefaultItemAnimator
