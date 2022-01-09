4826278321758b87b3bde0862de47249f3326bee
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2022-01-09 23:05:44 +0800

    Helper for getting sharedPreference values

--- app/src/main/java/org/grapheneos/apps/client/ui/settings/MainSettings.kt
+++ app/src/main/java/org/grapheneos/apps/client/ui/settings/MainSettings.kt
@@ -1,5 +1,7 @@
 package org.grapheneos.apps.client.ui.settings
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +10,29 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.preference.PreferenceFragmentCompat
 import org.grapheneos.apps.client.R
+import org.grapheneos.apps.client.App
 import org.grapheneos.apps.client.utils.sharedPsfsMgr.JobPsfsMgr
 
 class MainSettings : PreferenceFragmentCompat() {
+
+    companion object {
+        private val AUTO_UPDATE_INTERVAL_KEY = App.getString(R.string.seamlessUpdateInterval)
+        private val AUTO_INSTALL_UPDATES_KEY = App.getString(R.string.seamlessUpdateEnabled)
+        private val CHECK_FOR_UPDATES_NOW_KEY = App.getString(R.string.checkForUpdatesNow)
+
+        private val AUTO_UPDATE_INTERVAL_DEFAULT = App.getString(R.string.auto_download_interval_default)
+
+        private fun getPreferences(context: Context): SharedPreferences {
+            return context.getSharedPreferences(JobPsfsMgr.AUTO_UPDATE_PREFERENCE, Context.MODE_PRIVATE)
+        }
+
+        fun getAutoUpdateDownloadedApps(context: Context) = getPreferences(context).getBoolean(
+            AUTO_INSTALL_UPDATES_KEY, context.resources.getBoolean(R.bool.auto_update_default))
+
+        fun getAutoUpdateInterval(context: Context): Long = (getPreferences(context).getString(
+            AUTO_UPDATE_INTERVAL_KEY, AUTO_UPDATE_INTERVAL_DEFAULT) ?:
+            AUTO_UPDATE_INTERVAL_DEFAULT).toLong()
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         preferenceManager.sharedPreferencesName = JobPsfsMgr.AUTO_UPDATE_PREFERENCE
