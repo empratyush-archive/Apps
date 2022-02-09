dbb6f5337e10f6d98ec7b6f57d5b5b69c2f4de7a
empratyush <codelab@pratyush.dev>
2022-02-09 17:34:41 +0530

    added a hardcoded list of apps with it's category

---
+++ app/src/main/java/org/grapheneos/apps/client/utils/AppSourceHelper.kt
@@ -0,0 +1,27 @@
+package org.grapheneos.apps.client.utils
+
+class AppSourceHelper {
+
+    companion object {
+        private const val gos = "GrapheneOS"
+        private const val google = "Google (mirror)"
+        private const val mirror = "mirror"
+        private const val buildByGos = "ReBuild"
+
+
+        /*package name, category name, */
+        private val listOfKnownApps = mutableMapOf<String, String>().apply {
+            put("app.attestation.auditor", gos)
+            put("org.grapheneos.pdfviewer", gos)
+            put("app.grapheneos.camera", gos)
+
+            put("com.google.android.gms", google)
+            put("com.google.android.gsf", google)
+            put("com.android.vending", google)
+
+        }
+
+        fun getCategoryName(pkgName: String) = listOfKnownApps.getOrDefault(pkgName, gos)
+    }
+
+}
